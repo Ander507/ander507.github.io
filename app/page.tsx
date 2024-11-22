c7968eb26e0067e0235114ee16e729661e0e9a8e
Ander507 <99738972+Ander507@users.noreply.github.com>
2024-11-22 21:39:21 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { Sun, Moon, Github, Linkedin, Twitter } from 'lucide-react'
+import { Sun, Moon, Github, Twitter } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
